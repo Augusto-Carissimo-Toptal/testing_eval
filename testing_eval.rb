f3e34f56b74557e18d43f1c1fd89727076cfa159
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2022-12-23 12:20:37 -0300

    Add instructions

--- testing_eval.rb
+++ testing_eval.rb
@@ -1,1 +1,13 @@
-git 
+# Problem
+
+# You have a class called Map with attributes @height, @width, @cursor
+# You have a class called Cursor with attributes @x, @y
+# We want a method Map#move() that moves the cursor to the :left, :right, :up, :down
+# Map#move(symbol) -> Map with new cursor
+#5 |_|_|_|_|_|                             5 |_|_|_|_|_|
+#4 |_|_|_|_|_|                             4 |_|_|_|_|_|
+#3 |_|_|x|_|_|  ---> map.move(:left) --->  3 |_|x|_|_|_|
+#2 |_|_|_|_|_|                             2 |_|_|_|_|_|
+#1 |_|_|_|_|_|                             1 |_|_|_|_|_|
+#   1 2 3 4 5                                 1 2 3 4 5
+
