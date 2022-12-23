82e7f2f3e6849c8b4000c5a647b5012078273a53
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2022-12-23 12:32:12 -0300

    Add assert_exist and assert_kind_of in Test, add Map and Cursor

--- testing_eval.rb
+++ testing_eval.rb
@@ -10,4 +10,43 @@
 #2 |_|_|_|_|_|                             2 |_|_|_|_|_|
 #1 |_|_|_|_|_|                             1 |_|_|_|_|_|
 #   1 2 3 4 5                                 1 2 3 4 5
+# defined? Map
 
+
+class Map
+end
+
+class Cursor
+end
+
+class Test
+  
+  def assert_exist(_class)
+    if defined? _class
+      return 'Pass'
+    else
+      return <<~MSG
+      Class doesnt exist
+      MSG
+    end
+  end
+
+  def assert_kind_of(_class, expected_class)
+    if _class.class == expected_class
+      return 'Pass'
+    else
+      return <<~MSG
+      Expected class is: #{expected_class}
+      but instead is: #{_class}
+      MSG
+    end
+  end
+end
+
+test = Test.new
+
+puts test.assert_exist(Map)
+puts test.assert_exist(Cursor)
+
+puts test.assert_kind_of(Map.new, Map)
+puts test.assert_kind_of(Cursor.new, Cursor)
