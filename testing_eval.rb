77a6cf19ee56262800f95aeaf9a2836858a1f554
Augusto-Carissimo_Toptal <augusto.carissimo@toptal.com>
2022-12-23 13:12:06 -0300

    Add assert_Cursor_method to Test and add left, right, up, down method to Cursor

--- testing_eval.rb
+++ testing_eval.rb
@@ -12,7 +12,7 @@
 #   1 2 3 4 5                                 1 2 3 4 5
 # defined? Map
 
-
+#Map --------------------------------
 class Map
   attr_reader :height, :width, :cursor
 
@@ -22,20 +22,46 @@ class Map
     @cursor = cursor
   end
 
-  
-
 end
 
-class Cursor
 
+
+
+
+# Cursor -----------------
+class Cursor
   attr_reader :x, :y
 
   def initialize(x,y)
     @x = x
     @y = y
-  end    
+  end
+
+  def left
+    @x -= 1 
+    return [@x, @y]
+  end
+
+  def right
+    @x += 1 
+    return [@x, @y]
+  end
+
+  def up
+    @y += 1 
+    return [@x, @y]
+  end
+
+  def down
+    @y -= 1 
+    return [@x, @y]
+  end
 end
 
+
+
+
+# Test -------------------
 class Test
   
   def assert_exist(_class)
@@ -70,7 +96,28 @@ class Test
     end
   end
 
-  
+  def assert_Cursor_method(class_method, array_position)
+    if class_method == array_position
+      return 'Pass'
+    else
+      <<~MSG
+       Expected method to return: #{array_position}
+       Instead return:            #{class_method}
+       MSG
+    end
+  end
+
+  # def assert_move_Map(class_method, array_position)
+  #   if class_method == array_position
+  #     return 'Pass'
+  #   else
+  #     <<~MSG
+  #     Expected #move to return: #{array_position}
+  #     Instead return:           #{class_method}
+  #     MSG
+  #   end
+  # end
+
 
 end
 
@@ -87,4 +134,9 @@ puts test.assert_same(Map.new(5,5,5).width, 5) #width
 puts test.assert_same(Map.new(5,5,5).cursor, 5) #cursor
 puts test.assert_same(Cursor.new(1,1).x, 1) #x
 puts test.assert_same(Cursor.new(1,1).y, 1) #y
+
+puts test.assert_Cursor_method(Cursor.new(3,3).left, [2,3]) #left
+puts test.assert_Cursor_method(Cursor.new(3,3).right, [4,3]) #right
+puts test.assert_Cursor_method(Cursor.new(3,3).up, [3,4]) #up
+puts test.assert_Cursor_method(Cursor.new(3,3).down, [3,2]) #down
 
